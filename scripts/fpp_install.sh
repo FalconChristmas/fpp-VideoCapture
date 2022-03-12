a2fc56b1070f0a2b5763c63a0023bbfc08f18eef
Daniel Kulp <dan@kulp.com>
2022-03-12 15:35:07 -0500

    Add v4l stuff back to install

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -3,7 +3,7 @@
 # fpp-VideoCapture install script
 
 sudo apt-get -y update
-sudo apt-get -y install libcamera-dev
+sudo apt-get -y install libcamera-dev libv4l-dev v4l-utils
 
 BASEDIR=$(dirname $0)
 cd $BASEDIR
